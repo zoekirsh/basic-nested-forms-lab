4b78a428e507991096a61c4bac3ade9b3ad97d05
Ashi Krishnan <me@ashi.works>
2016-01-13 01:04:11 -0500

    Author/post migrations and relations.

---
+++ db/migrate/20160113060326_add_author_to_posts.rb
@@ -0,0 +1,5 @@
+class AddAuthorToPosts < ActiveRecord::Migration
+  def change
+    add_reference :posts, :author, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113004057) do
+ActiveRecord::Schema.define(version: 20160113060326) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -33,7 +33,10 @@ ActiveRecord::Schema.define(version: 20160113004057) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id"
   end
+
+  add_index "posts", ["author_id"], name: "index_posts_on_author_id"
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
